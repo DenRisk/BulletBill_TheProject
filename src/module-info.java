407a33d44534045544069298a0cfb3df85cf422d
DenRisk <35171305+DenRisk@users.noreply.github.com>
2019-05-03 13:28:25 +0200

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+module PhysikEngineTest {
+
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.media;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.swt;
+
+    opens main;
+
+}
